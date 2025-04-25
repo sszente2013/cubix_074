27fd4929fe8257c92e9f9c7882d78fdd4292fe1f
sszente2013 <84807189+sszente2013@users.noreply.github.com>
2025-04-25 18:43:57 +0200

    Create test_szamtan.py

---
+++ test_szamtan.py
@@ -0,0 +1,13 @@
+from szamtan import *
+
+def test_osszead():
+    assert osszead(2,3) == 5
+
+def test_kivon():
+    assert kivon(2, 3) == -1
+
+def test_szoroz():
+    assert szoroz(2, 3) == 6
+
+def test_oszt():
+    assert oszt(10,5) == 2
